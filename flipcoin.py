25a75c7813c1bda7a20659aa34b0af0e304f6f5d
thenotoriousmac <mccaskied@gmail.com>
2020-04-27 13:17:58 -0400

    Small Changes

--- flipcoin.py
+++ flipcoin.py
@@ -1,5 +1,5 @@
 from random import choice
 
 async def run(main):
-    options = ["HEADS", "TAILS"]
+    options = ["HEADS :simple_smile:", "TAILS \U0001F643"]
     await main.channel.send(choice(options))
