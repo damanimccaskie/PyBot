97bd797160641689e7f5497d9ba0208253fc5ee9
JChris246 <chris.401@live.com>
2020-05-17 22:31:48 -0400

    Fixed issue with bot detecting bad words in other words
    Bot was detecting bad words even when they were part of other words (not stand alone)

--- bot.py
+++ bot.py
@@ -75,15 +75,19 @@ async def on_message(message):
     bad_words = ["nigger", "covid", "school", "project", "porn", "paper", "research", "uwi", "se",
     "corona", "paul", "walcot", "lowe", "hilary"]
 
-    if message.author == client.user:
+    if message.author == client.user: # dont check for bad words of bot (can lead to infinite loop)
       return
 
    
-
+    def find(msg, bad): #linear search
+        for i in range(0, len(msg)):
+            if msg[i:i+len(bad)] == bad and (i - 1 < 0 or msg[i-1] == ' ') and (i+len(bad) >= len(msg) or msg[i+len(bad)] == ' '):
+                return True
+        return False
 
     for word in bad_words:
-        if message.content.lower().find(word) != -1:
-            print("A bad word was said ")
+        if find(message.content.lower(), word):
+            print("A bad word was said")
             await message.channel.send("Stop cursing you little fucking nigger!")
             break
 
